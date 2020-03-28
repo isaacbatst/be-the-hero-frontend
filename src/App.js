c2a73823bdebed98ed723a64f1b2131a6ece6ad6
Isaac Batista <isaacbatista@rits.com.br>
2020-03-28 14:57:32 -0300

    create and stylize logon component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logon from './pages/Logon';
+
+import './global.css';
 
 function App() {
+
   return (
-    <h1>
-      hey
-    </h1>
+      <Logon />
   );
 }
 
